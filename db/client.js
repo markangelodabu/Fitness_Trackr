b30fd5dde55cabde638d89666f595fc7d9657e1c
Mark Angelo Dabu <markangelo.dabu@gmail.com>
2022-02-15 21:01:23 -0800

    Built and export unconnected client

--- db/client.js
+++ db/client.js
@@ -1,2 +1,8 @@
 // build and export your unconnected client here
+const {Client} = require('pg');
+const CONNECTION_STRING = "postgres://localhost:5432/fitness-dev";
+const client = new Client(CONNECTION_STRING);
 
+module.exports = {
+    client
+}
