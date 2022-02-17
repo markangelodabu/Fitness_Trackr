ab4c6e2e7727b2861d3b2de58399588c0c4ec944
DonnyDunamis <mjd_mail@yahoo.com>
2022-02-16 21:28:51 -0800

    completed users.js basic housekeeping

--- db/users.js
+++ db/users.js
@@ -1,21 +1,96 @@
-const client = require('./client');
+const { client } = require("./client");
+const bcrypt = require("bcrypt");
+const SALT_ROUNDS = 10;
 
-async function createUser({username, password}) {
-    try {
-        const { rows: [user] } = await client.query(`
+const getUsers = async () => {
+  const { rows: users } = await client.query(`
+        SELECT * FROM users;
+    `);
+  return users;
+};
+
+const createUser = async ({ username, password }) => {
+  const hashPwd = await bcrypt.hash(password, SALT_ROUNDS);
+  try {
+    const {
+      rows: [user],
+    } = await client.query(
+      `
             INSERT INTO users(username, password)
             VALUES ($1, $2)
-            ON CONFLICT (username) DO NOTHING
             RETURNING *;
-        `,[username, password]);
-
-        return user;  
-    }   catch (error) {
-        throw error;
+        `,
+      [username, hashPwd]
+    );
+    console.log(user);
+    return user;
+  } catch (error) {
+    throw error;
+  }
+};
+const getUser = async ({ username, password }) => {
+  try {
+    const {
+      rows: [user],
+    } = await client.query(
+      `
+        SELECT * FROM users
+        WHERE username = $1;
+      `,
+      [username, password]
+    );
+    console.log("Before return", user);
+    if (!user) {
+      throw { name: "userNotFound", message: "User not found" };
     }
-}
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (isMatch) {
+      delete user.password;
+      return user;
+    }
+  } catch (error) {
+    console.log("Error with getUser");
+    throw error;
+  }
+};
+const getUserById = async (id) => {
+  try {
+    const {
+      rows: [user],
+    } = await client.query(
+      `
+      SELECT id, username, password
+      FROM users
+      WHERE id = $1;
+      `,
+      [id]
+    );
+    delete user.password;
+    console.log("This is getUserById", user);
+    return user;
+  } catch (error) {
+    console.log("Error with getUserById");
+    throw error;
+  }
+};
+const getUserByUsername = async (username) => {
+  try {
+      const {rows: [user] } = await client.query(`
+        SELECT * FROM users
+        WHERE username = $1;
+      `, [username]);
+      return user;
+  } catch (error) {
+    console.log("Error with getUserByUsername");
+    throw error;
+  }
+};
 
 module.exports = {
-    client,
-    createUser
-}
+  client,
+  createUser,
+  getUsers,
+  getUser,
+  getUserById,
+  getUserByUsername,
+};
--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // create the express server here
+const express = require('express');
+const server = express();
+const morgan = require('morgan');
+const { client } = require('./db/client');
+require('dotenv').config();
+const {PORT} = process.env;
+
+
+server.listen(PORT, () => {
+    client.connect();
+    console.log(`Listening on http://localhost:${PORT}`);
+})
