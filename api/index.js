e462635321f1e9ad3a01081f32b137f47a94ab83
DonnyDunamis <mjd_mail@yahoo.com>
2022-02-25 20:14:36 -0800

    updated /api/activities and /api/users

--- api/index.js
+++ api/index.js
@@ -4,21 +4,18 @@ const apiRouter = express.Router();
 
 // attach other routers from files in this api directory (users, activities...)
 
+apiRouter.get("/health", async (req, res, next) => {
+  res.send({
+    message: "All is well"
+  });
+});
 const usersRouter = require("./users");
 const routinesRouter = require("./routines");
 const activitiesRouter = require("./activities");
-
 
 apiRouter.use("/users", usersRouter);
 apiRouter.use("/routines", routinesRouter);
 apiRouter.use("/activities", activitiesRouter);
 
-apiRouter.get("./health", async(req, res, next) => {
-    res.send({
-        message: "All is well"
-    })
-})
-
 // export the api router
 module.exports = apiRouter;
-
